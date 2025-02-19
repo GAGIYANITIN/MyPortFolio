bff34edb22905ff7a48ef2ea1050e9916322554c
GAGIYANITIN <141313724+GAGIYANITIN@users.noreply.github.com>
2025-02-19 11:17:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('languagee').addEventListener('click', function() {
+            // Scroll to the section with the ID 'section2'
+            document.getElementById('languagel').scrollIntoView({ behavior: 'smooth' });
+        }
+);
+document.getElementById('profilee').addEventListener('click', function() {
+            // Scroll to the section with the ID 'section2'
+            document.getElementById('profilep').scrollIntoView({ behavior: 'smooth' });
+        }
+);
+document.getElementById('skilll').addEventListener('click', function() {
+            // Scroll to the section with the ID 'section2'
+            document.getElementById('skillsk').scrollIntoView({ behavior: 'smooth' });
+        }
+);
+document.getElementById('projectt').addEventListener('click', function() {
+            // Scroll to the section with the ID 'section2'
+            document.getElementById('projectp').scrollIntoView({ behavior: 'smooth' });
+        }
+);
